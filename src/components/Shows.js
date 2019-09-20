0f4d5d7fd42c92ad4af274d34108339a3c6b057b
Oscar <oscar.cabrera@ntsprint.com>
2019-09-20 11:15:53 -0400

    added collapsible panels

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -1,21 +1,26 @@
-import Avatar from "@material-ui/core/Avatar"
-import Divider from "@material-ui/core/Divider"
-import List from "@material-ui/core/List"
-import ListItem from "@material-ui/core/ListItem"
-import ListItemAvatar from "@material-ui/core/ListItemAvatar"
-import ListItemText from "@material-ui/core/ListItemText"
-import Typography from "@material-ui/core/Typography"
-import { makeStyles } from "@material-ui/styles"
-import React from "react"
+import Avatar from "@material-ui/core/Avatar";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from "@material-ui/styles";
+import React from "react";
 
 const useStyles = makeStyles(theme => ({
   root: {
     width: "100%",
     maxWidth: 768,
-    backgroundColor: theme.palette.background.paper
   },
-  inline: {
-    display: "inline"
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+    display: "flex",
+    alignItems: "center",
+    paddingLeft:10
+  },
+  showName:{
+   
   }
 }))
 
@@ -23,12 +28,15 @@ export default function AlignItemsList(props) {
   const classes = useStyles()
 
   return (
-    <List className={classes.root}>
+    <div  className={classes.root}>
       {props.data.map(item => (
-        <div key={item.show.id}>
-          <ListItem alignItems="center">
-            <ListItemAvatar>
-              <Avatar
+        <ExpansionPanel key={item.show.id}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Avatar
                 alt={item.show.name}
                 src={
                   item.show.image
@@ -36,36 +44,65 @@ export default function AlignItemsList(props) {
                     : "https://ui-avatars.com/api/?name=" + item.show.name
                 }
               />
-            </ListItemAvatar>
-            <ListItemText
-              primary={item.show.name}
-              secondary={
-                <React.Fragment>
-                  <Typography
-                    component="span"
-                    variant="body2"
-                    className={classes.inline}
-                    color="textPrimary"
-                  >
-                    {item.show.genres.map(genre => genre + " ")}
-                  </Typography>
-                  {item.show.summary &&
-                    item.show.summary
-                      .replace(/<[/]?p>/g, "")
-                      .replace(/<[/]?b>/g, "")
-                      .replace(/<[/]?i>/g, "")}
-                </React.Fragment>
-              }
-            />
-          </ListItem>
-          <Divider variant="inset" component="li" />
-        </div>
+            <Typography className={classes.heading}>
+              {item.show.name}
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              {item.show.summary &&
+                item.show.summary
+                  .replace(/<[/]?p>/g, "")
+                  .replace(/<[/]?b>/g, "")
+                  .replace(/<[/]?i>/g, "")}
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       ))}
-    </List>
+    </div>
+    // <List className={classes.root}>
+    //   {props.data.map(item => (
+    //     <div key={item.show.id}>
+    //       <ListItem alignItems="center">
+    //         <ListItemAvatar>
+    //           <Avatar
+    //             alt={item.show.name}
+    //             src={
+    //               item.show.image
+    //                 ? item.show.image.medium
+    //                 : "https://ui-avatars.com/api/?name=" + item.show.name
+    //             }
+    //           />
+    //         </ListItemAvatar>
+    //         <ListItemText
+    //           primary={item.show.name}
+    //           secondary={
+    //             <React.Fragment>
+    //               <Typography
+    //                 component="span"
+    //                 variant="body2"
+    //                 className={classes.inline}
+    //                 color="textPrimary"
+    //               >
+    //                 {item.show.genres.map(genre => genre + " ")}
+    //               </Typography>
+    //               {item.show.summary &&
+    //                 item.show.summary
+    //                   .replace(/<[/]?p>/g, "")
+    //                   .replace(/<[/]?b>/g, "")
+    //                   .replace(/<[/]?i>/g, "")}
+    //             </React.Fragment>
+    //           }
+    //         />
+    //       </ListItem>
+    //       <Divider variant="inset" component="li" />
+    //     </div>
+    //   ))}
+    // </List>
   )
 }
 
 AlignItemsList.defaultProps = {
   data: [],
-  query:""
-};
+  query: ""
+}
